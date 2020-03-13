31297712e9fe2ba6c8ff0368b0478bec19bd325d
zswj123 <l2se@sina.cn>
2020-03-14 06:51:29 +0800

    编译文档，部署文档到静态服务器。

--- deploy_doc.sh
+++ deploy_doc.sh
@@ -1,1 +1,6 @@
-scp -r ./doc/_build/html wobuhuizaibeishang@serv_pro:~/static_server/MingMQgi
+cd ./doc
+make clean
+sphinx-apidoc -o ./ ../mingmq/
+make html
+cd ..
+scp -r ./doc/_build/html wobuhuizaibeishang@serv_pro:~/static_server/MingMQ
